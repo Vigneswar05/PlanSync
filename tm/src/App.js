33e16371f9739ca895aa6cb8f22a7e7824f776a1
Your Full Name <your.email@example.com>
2025-06-18 03:32:59 +0530

    Configured GitHub Pages deployment

--- tm/src/App.js
+++ tm/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login'; 
